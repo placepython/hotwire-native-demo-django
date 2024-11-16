f6294e2d25ddbd48dd6dbee89a2051b64fd25ac2
Thierry Chappuis <tchappui@users.noreply.github.com>
2024-11-16 23:23:38 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from config import settings # noqa F401
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
